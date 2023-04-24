23fc5d95ee78bfab9496db7752e7de77a141b384
Mohamed AJJAJI <majjaji98@gmail.com>
2023-04-23 18:45:56 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,59 +1,51 @@
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
  * _printf - produces output according to a format
  * @format: string containing zero or more directives
+ *
  * Return: Number of Characters Printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
-	char c, *s;
+	int printed_chars = 0, i;
+	char *str_arg, *p, buf[1024];
 
 	va_start(args, format);
-	if (format == NULL)
-		return (-1);
-	while (*format)
+
+	for (p = (char *)format; *p != '\0'; p++)
 	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-			case 'c':
-				c = va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-				break;
-			case 's':
-				s = va_arg(args, char*);
-				while (*s)
-				{
-					write(1, s, 1);
-					s++;
-					count++;
-				}
-				break;
-			case '%':
-				write(1, format, 1);
-				count++;
-				break;
-			default:
-				va_end(args);
-				return (-1);
-			}
-		}
+		if (*p != '%')
+			write(1, p, 1), printed_chars++;
 		else
 		{
-			write(1, format, 1);
-			count++;
+			switch (*++p)
+			{
+				case 'c':
+					i = va_arg(args, int), buf[0] = i;
+					write(1, buf, 1), printed_chars++;
+					break;
+				case 's':
+					str_arg = va_arg(args, char *);
+					if (str_arg == NULL)
+						str_arg = "(null)";
+					for (i = 0; str_arg[i] != '\0'; i++)
+					{
+						buf[i] = str_arg[i];
+					}
+					write(1, buf, i), printed_chars += i;
+					break;
+				case '%':
+					buf[0] = '%', write(1, buf, 1), printed_chars++;
+					break;
+				default:
+					buf[0] = '%', buf[1] = *p;
+					write(1, buf, 2), printed_chars += 2;
+					break;
+			}
 		}
-		format++;
 	}
 	va_end(args);
-	return (count);
+	return (printed_chars);
 }
--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
